f0d40b166fd0f74f8f4891799c9a1219f3254d2a
anup-majhi123 <anup.malaysia2023@gmail.com>
2026-01-04 23:05:13 +0800

    hi

--- a.py
+++ a.py
@@ -1,1 +1,13 @@
 print("hello")
+print(4+5)
+
+class Car:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+    def print_car(self):
+        print(self.name)
+        print(self.price)
+        
+bmw = Car("BMW", 1200)
+bmw.print_car()
